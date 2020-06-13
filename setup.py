3364e25ae000a32ebef1ab2a6a97053691ad5dee
Jo Bovy <bovy@astro.utoronto.ca>
2020-06-12 20:52:25 -0400

    Add basic structure

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="streamtools",
+    version="0.1",
+    author="Jo Bovy",
+    author_email="bovy@astro.utoronto.ca",
+    description="Tools for modeling and analyzing stellar streams",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    packages=["streamtools","streamtools/df"],
+    install_requires=['galpy']
+    )
